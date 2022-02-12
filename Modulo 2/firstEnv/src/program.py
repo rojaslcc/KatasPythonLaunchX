87cec41748fa165b52f86fc2ca1f75105a116a92
Humberto Rojas (H.R.) <rojaslcc@msn.com>
2022-02-12 13:39:09 -0600

    fisrt commit

---
+++ Modulo 2/firstEnv/src/program.py
@@ -0,0 +1,20 @@
+from datetime import *
+from dateutil.relativedelta import *
+now = datetime.now()
+
+print("Hola Mundo!!!")
+print(now)
+
+now = now + relativedelta(months=1, weeks=1, hour=10)
+print(now)
+
+"""
+Katas 1 Modulo 2: Lic. Jesus Humberto Rojas Rangel
+ Fecha: 2022/02/09
+
+Mis Redes:
+* GitHub: [rojaslcc](https://www.github.com/rojaslcc/)
+* Linkedin: [rojaslcc](https://www.linkedin.com/in/rojaslcc/)
+* Twitter: [@rojaslcc](https://www.twitter.com/rojaslcc/)
+* Instagram: [rojaslcc](https://www.instagram.com/rojaslcc/)
+"""
---
+++ README.md
@@ -0,0 +1,8 @@
+<em>Katas elaboradas por: Lic. Jesus Humberto Rojas Rangel
+<br/> Last Commit: 2022/02/12 - 01:40 PM</em>
+
+Mis Redes:
+* GitHub: [rojaslcc](https://www.github.com/rojaslcc/)
+* Linkedin: [rojaslcc](https://www.linkedin.com/in/rojaslcc/)
+* Twitter: [@rojaslcc](https://www.twitter.com/rojaslcc/)
+* Instagram: [rojaslcc](https://www.instagram.com/rojaslcc/)
